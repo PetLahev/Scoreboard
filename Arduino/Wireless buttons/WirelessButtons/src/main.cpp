9ce339ee4478f003bf0dddac7b2e38d4d7bdeb84
PetLahev <petlahev@hotmail.com>
2019-05-01 22:01:05 +0200

    added code for all buttons

--- Arduino/Wireless buttons/WirelessButtons/src/main.cpp
+++ Arduino/Wireless buttons/WirelessButtons/src/main.cpp
@@ -22,21 +22,81 @@
 #define COMMUNICATION_PIN 12
 
 // Instance of the button.
-EasyButton button(BTN_HOME_UP);
+EasyButton btnHomeUp(BTN_HOME_UP);
+EasyButton btnHomeDown(BTN_HOME_DOWN);
+EasyButton btnAwayUp(BTN_AWAY_UP);
+EasyButton btnAwayDown(BTN_AWAY_DOWN);
+EasyButton btnHomePlayer(BTN_HOME_PLAYER);
+EasyButton btnAwayPlayer(BTN_AWAY_PLAYER);
+EasyButton btnReset(BTN_RESET_ALL);
+EasyButton btnGame1(BTN_GAME_1);
+EasyButton btnGame2(BTN_GAME_2);
+EasyButton btnGame3(BTN_GAME_3);
+EasyButton btnGame4(BTN_GAME_4);
 
-// Callback function to be called when the button is pressed.
-void onPressed() {
-	
-  char *message[128];  
-
+void sendMessage(char* message) {
+  
   digitalWrite(13, true);  
-  vw_send((uint8_t *)'h', 128);  
+  vw_send((uint8_t *)message, strlen(message));  
   vw_wait_tx();  
   digitalWrite(13, false);  
-  delay(100);
+  delay(1000);
+}
+
+// Callback functions 
+void onHomeUpPressed() {	
+  Serial.println("Home up pressed");
+  sendMessage("h");
+}
+
+void onHomeDownPressed() {	
+  Serial.println("Home down pressed");
+  sendMessage("v");
+}
+
+void onAwayUpPressed() {	
+  Serial.println("Away up pressed");
+  sendMessage("a");
+}
+
+void onAwayDownPressed() {	
+  Serial.println("Away down pressed");
+  sendMessage("m");
+}
+
+void onHomePlayerPressed() {	
+  Serial.println("Home player pressed");
+}
+
+void onAwayPlayerPressed() {	
+  Serial.println("Away player pressed");
+}
+
+void onGame1Pressed() {	
+  Serial.println("Game 1 pressed");
+}
+
+void onGame2Pressed() {	
+  Serial.println("Game 2 pressed");
+}
+
+void onGame3Pressed() {	
+  Serial.println("Game 3 pressed");
+}
+
+void onGame4Pressed() {	
+  Serial.println("Game 4 pressed");
+}
+
+void onResetPressed() {	
+  Serial.println("Reset pressed");
+  sendMessage("@");
 }
 
 void setup() {  
+
+  Serial.begin(9600);
+  Serial.println("Initializing");
 
   // set the type of communication
   vw_set_ptt_inverted(true);
@@ -44,10 +104,45 @@ void setup() {
   vw_setup(1000);  
   vw_set_tx_pin(COMMUNICATION_PIN);
 
-  button.begin();
-  button.onPressed(onPressed);
+  Serial.println("Communication set up");
+
+  btnHomeUp.begin();
+  btnHomeDown.begin();
+  btnAwayUp.begin();
+  btnAwayDown.begin();
+  btnHomePlayer.begin();
+  btnAwayPlayer.begin();
+  btnReset.begin();
+  btnGame1.begin();
+  btnGame2.begin();
+  btnGame3.begin();
+  btnGame4.begin();
+
+  btnHomeUp.onPressed(onHomeUpPressed);
+  btnHomeDown.onPressed(onHomeDownPressed);
+  btnAwayUp.onPressed(onAwayUpPressed);
+  btnAwayDown.onPressed(onAwayDownPressed);
+  btnHomePlayer.onPressed(onHomePlayerPressed);
+  btnAwayPlayer.onPressed(onAwayPlayerPressed);  
+  btnGame1.onPressed(onGame1Pressed);
+  btnGame2.onPressed(onGame2Pressed);
+  btnGame3.onPressed(onGame3Pressed);
+  btnGame4.onPressed(onGame4Pressed);
+  btnReset.onPressedFor(5000, onResetPressed);
+
+  Serial.println("Buttons initialized");
 }
 
 void loop() {
-  button.read();
+  btnHomeUp.read();
+  btnHomeDown.read();
+  btnAwayUp.read();
+  btnAwayDown.read();
+  btnHomePlayer.read();
+  btnAwayPlayer.read();
+  btnReset.read();
+  btnGame1.read();
+  btnGame2.read();
+  btnGame3.read();
+  btnGame4.read();
 }
